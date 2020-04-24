a039d616f0d2b8b1bb6dd205e24f3415e09a3a76
KerollosNabil <kokonabil4444@gmail.com>
2020-04-24 08:00:25 -0700

    Initial Commit

---
+++ EmojiArt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiArt
+//
+//  Created by kerollos nabil on 4/24/20.
+//  Copyright © 2020 kerollos nabil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
