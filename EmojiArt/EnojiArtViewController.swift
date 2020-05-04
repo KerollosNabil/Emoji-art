921443e4e5641a6950290cc386a801bb9b8a2edd
KerollosNabil <kokonabil4444@gmail.com>
2020-05-04 12:20:45 -0700

    make image zoomable and add collection view

--- EmojiArt/EnojiArtViewController.swift
+++ EmojiArt/EnojiArtViewController.swift
@@ -8,16 +8,53 @@
 
 import UIKit
 
-class EnojiArtViewController: UIViewController, UIDropInteractionDelegate {
+class EnojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScrollViewDelegate {
 
+    var emojiAetView = EmojiArtView()
+    var imageFeacher:ImageFetcher!
+    
     @IBOutlet weak var drobZone: UIView!{
         didSet{
             drobZone.addInteraction(UIDropInteraction(delegate: self))
         }
     }
-    @IBOutlet weak var emojiAetView: EmojiArtView!
-    var imageFeacher:ImageFetcher!
+    @IBOutlet weak var imageScrollView: UIScrollView!{
+        didSet{
+            imageScrollView.maximumZoomScale = 5.0
+            imageScrollView.minimumZoomScale = 0.1
+            imageScrollView.delegate = self
+            imageScrollView.addSubview(emojiAetView)
+
+        }
+    }
+    @IBOutlet weak var widthOfScrollView: NSLayoutConstraint!
     
+    @IBOutlet weak var hightOfScrollView: NSLayoutConstraint!
+    
+    
+    var emojiArtBackgeound: UIImage? {
+        get{
+            return emojiAetView.backGround
+        }set{
+            imageScrollView.zoomScale = 1.0
+            emojiAetView.backGround = newValue
+            let size = newValue?.size ?? CGSize.zero
+            emojiAetView.frame = CGRect(origin: CGPoint.zero, size: size)
+            imageScrollView.contentSize = size
+            widthOfScrollView.constant = size.width
+            hightOfScrollView.constant = size.height
+            if size.width > 0, size.height > 0{
+                imageScrollView.zoomScale = max(drobZone.bounds.width/size.width, drobZone.bounds.height/size.height)
+            }
+        }
+    }
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        widthOfScrollView.constant = scrollView.contentSize.width
+        hightOfScrollView.constant = scrollView.contentSize.height
+    }
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return emojiAetView
+    }
     func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
         return session.canLoadObjects(ofClass: NSURL.self) && session.canLoadObjects(ofClass: UIImage.self)
     }
@@ -27,7 +64,7 @@ class EnojiArtViewController: UIViewController, UIDropInteractionDelegate {
     func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
         imageFeacher = ImageFetcher(){(url, image) in
             DispatchQueue.main.async {
-                self.emojiAetView.backGround = image
+                self.emojiArtBackgeound = image
             }
             
         }
